3d4602ac448fbc9f4cc7929e3a839d9ec983dc7b
Angus Feng <z5207356@unsw.edu.au>
2019-09-17 19:07:14 +1000

    added pokelist

---
+++ test.js
@@ -0,0 +1,50 @@
+function test() {
+    const pokeList = document.querySelector("#pokeList");
+    console.log(pokeList);
+
+    const options = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        }
+    }
+
+    for (let i = 1; i <= 10; i++) {
+        let url =  `https://pokeapi.co/api/v2/pokemon/${i}/`;
+        fetch(url, options)
+            .then(response => response.json())
+            .then(JSON => {
+                const pokemon =  document.createElement("li");
+                pokemon.innerText = JSON.name;
+
+                pokeList.appendChild(pokemon);
+                console.log(pokemon);
+            });
+    }
+}
+
+test();
+
+/*
+const token = sessionStorage.getItem("token");
+let url = `${API_URL}/post/public`;
+if (token !== null) {
+    url = `${API_URL}/user/feed?p=${p}&n=${n}`;
+}
+
+const options = {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Token ${token}`
+    }
+}
+
+fetch(url, options)
+    .then(response => response.json())
+    .then(JSON => {
+        console.log(JSON);
+    }
+});
+
+*/
